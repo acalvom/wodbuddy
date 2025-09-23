d90b47024586487021e86bc94ab8cefed3c455be
acalvom <andreac.3108@gmail.com>
2025-09-23 10:04:24 +0200

    ✨: Add .nvmrc and update configuration files for improved project setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import {biomePlugin} from '@pbr1111/vite-plugin-biome';
-import {defineConfig} from "vite";
+import { biomePlugin } from '@pbr1111/vite-plugin-biome';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react(), tailwindcss(), biomePlugin()]
+	plugins: [react(), tailwindcss(), biomePlugin()],
+	resolve: {
+		alias: { '@': path.resolve(__dirname, './src') }
+	}
 });
