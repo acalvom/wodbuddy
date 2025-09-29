9b6458ac83b2c489136e578db13ff1e0e987939e
acalvom <andreac.3108@gmail.com>
2025-09-29 12:29:49 +0200

    ✨: Add HomePage component and integrate MovementsList into app router

---
+++ src/app/pages/home.page.tsx
@@ -0,0 +1,10 @@
+import { MovementsList } from '@/features/movements/ui/components/movements-list.component.tsx';
+
+export const HomePage = () => {
+	return (
+		<div>
+			<h1>Home Page</h1>
+			<MovementsList />
+		</div>
+	);
+};
--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AppLayout } from '@/app/layouts/app.layout.tsx';
+import { HomePage } from '@/app/pages/home.page.tsx';
 import { LoginPage } from '@/features/auth/ui/pages/login.page.tsx';
 import { SignupPage } from '@/features/auth/ui/pages/signup.page.tsx';
 import { RequireAuth } from '@/router/routes/require-auth.tsx';
@@ -20,7 +21,7 @@ export function AppRouter() {
 				},
 				{
 					element: <RequireAuth />,
-					children: [{ path: '/home', element: <h2>Home - Protected</h2> }]
+					children: [{ path: '/home', element: <HomePage /> }]
 				},
 				{
 					path: '/*',
