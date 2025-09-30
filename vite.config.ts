48aba88ca1b124cbcf389647b0b4f0f02c9ecca8
acalvom <andreac.3108@gmail.com>
2025-09-30 12:45:05 +0200

    🧪: Configure Vitest setup and enhance TypeScript settings for improved testing

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { biomePlugin } from '@pbr1111/vite-plugin-biome';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [react(), tailwindcss(), biomePlugin()],
 	resolve: {
 		alias: { '@': path.resolve(__dirname, './src') }
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './vitest.setup.ts',
+		exclude: [...configDefaults.exclude]
 	}
 });
