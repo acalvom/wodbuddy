f493a583adb91b415a536dc0358fc3038c5909e5
acalvom <andreac.3108@gmail.com>
2025-10-03 14:03:03 +0200

    ✨: Enhance MovementsList with Loading component and add navigation button in HomePage for creating new movements

--- src/app/pages/home.page.tsx
+++ src/app/pages/home.page.tsx
@@ -1,5 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@/common/ui/shade-ui/components/ui/button.tsx';
 import { MovementsList } from '@/features/movements/ui/components/movements-list/movements-list.component.tsx';
 
 export const HomePage = () => {
-	return <MovementsList />;
+	const navigate = useNavigate();
+	return (
+		<div>
+			<Button
+				type="button"
+				variant="outline"
+				onClick={() => navigate('/create-movement')}
+				className="btn btn-primary mb-4"
+			>
+				Añadir movimiento
+			</Button>
+			<MovementsList />
+		</div>
+	);
 };
--- src/features/movements/ui/components/movements-list/movements-list.component.tsx
+++ src/features/movements/ui/components/movements-list/movements-list.component.tsx
@@ -1,15 +1,16 @@
-import { Loader2Icon } from 'lucide-react';
+import { Loading } from '@/common/ui/custom-components/loading/loading.tsx';
 import { MovementCard } from '@/features/movements/ui/components/movement-card/movement-card.component.tsx';
 import { useGetMovements } from '@/features/movements/ui/controllers/use-get-movements.hook.ts';
 
 export const MovementsList = () => {
 	const { data: movements, isPending, isError } = useGetMovements();
 
+	if (isPending) return <Loading />;
+	if (isError) return <div>Error loading movements</div>;
+
 	return (
 		<div>
-			{isPending && <Loader2Icon className="animate-spin" />}
-			{isError && 'Error loading movements'}
-			{movements && movements?.length > 0
+			{movements && movements.length > 0
 				? movements.map((movement) => <MovementCard key={movement.id} movement={movement} />)
 				: 'No movements to show'}
 		</div>
