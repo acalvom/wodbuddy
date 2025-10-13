abf5ab1025d7e502da8a716ed91f300ccbf6ddf9
acalvom <andreac.3108@gmail.com>
2025-10-13 12:17:29 +0200

    ✨: Refactor AddMarkForm and AddMarkPage to use ZodNewMark for improved type safety and consistency

--- src/features/marks/ui/components/add-mark-form/add-mark-form.component.tsx
+++ src/features/marks/ui/components/add-mark-form/add-mark-form.component.tsx
@@ -5,26 +5,24 @@ import { InputFormItem } from '@/common/ui/custom-components/form-items/input-fo
 import { SubmitButton } from '@/common/ui/custom-components/form-items/submit-button-item';
 import { useConfetti } from '@/common/ui/hooks/use-confetti.hook.ts';
 import { Form } from '@/common/ui/shade-ui/components/ui/form';
-import { type ZodAddMark, ZodAddMarkFormSchema } from '../../models/zod-mark';
+import { type ZodNewMark, ZodNewMarkFormSchema } from '../../models/zod-new-mark';
 
 type AddMarkFormProps = {
-	onSubmit: (data: ZodAddMark) => void;
+	onSubmit: (data: ZodNewMark) => void;
 	isPending?: boolean;
 };
 
 export const AddMarkForm = ({ onSubmit, isPending = false }: AddMarkFormProps) => {
-	const { triggerConfetti } = useConfetti();
 
-	const form = useForm<ZodAddMark>({
-		resolver: zodResolver(ZodAddMarkFormSchema),
+	const form = useForm<ZodNewMark>({
+		resolver: zodResolver(ZodNewMarkFormSchema),
 		defaultValues: {
 			value: '',
 			createdOn: undefined
 		}
 	});
 
-	const handleSubmit = (data: ZodAddMark) => {
-		triggerConfetti();
+	const handleSubmit = (data: ZodNewMark) => {
 		onSubmit(data);
 	};
 
@@ -32,9 +30,7 @@ export const AddMarkForm = ({ onSubmit, isPending = false }: AddMarkFormProps) =
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(handleSubmit)} className="glass w-full max-w-md space-y-6 p-6">
 				<InputFormItem control={form.control} name="value" label="¿Peso?" placeholder="🏋🏻 102.5 kg" />
-
 				<DatePickerFormItem control={form.control} name="createdOn" label="¿Cuándo?" placeholder="DD/MM/YYYY" />
-
 				<SubmitButton isLoading={isPending}>Añadir marca</SubmitButton>
 			</form>
 		</Form>
--- src/features/marks/ui/pages/add-mark.page.tsx
+++ src/features/marks/ui/pages/add-mark.page.tsx
@@ -1,27 +1,42 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Parser } from '@/common/domain/parser/parser';
 import { Title } from '@/common/ui/custom-components/title/title';
+import { Toast } from '@/common/ui/custom-components/toast/toast';
+import { useConfetti } from '@/common/ui/hooks/use-confetti.hook';
 import { useAuth } from '@/features/auth/ui/hooks/use-auth.hook';
 import { AddMarkForm } from '../components/add-mark-form/add-mark-form.component';
-import type { ZodAddMark } from '../models/zod-mark';
+import { useAddNewMark } from '../controllers/use-add-new-mark.hook';
+import type { ZodNewMark } from '../models/zod-new-mark';
+import { zodToDomain } from '../models/zod-to-domain.mapper';
 
 export const AddMarkPage = () => {
+	const { id } = useParams();
+	const movementId = Parser.toInt(id!);
 	const { user } = useAuth();
-	// const { mutateAsync: addMovement, isPending, error } = useAddNewMark();
+	const { mutateAsync: addNewMark, isPending, isSuccess, error } = useAddNewMark();
+	const { triggerConfetti } = useConfetti();
 
-	const onSubmit = async (data: ZodAddMark) => {
+	const onSubmit = async (data: ZodNewMark) => {
 		if (!user?.id) return;
 		try {
-			console.log(data);
-			// const newMovement = zodToDomain(data, user.id);
-			// return await addMovement(newMovement);
+			const newMark = zodToDomain(data, user.id, movementId!);
+			return await addNewMark(newMark);
 		} catch {}
 	};
+
+	useEffect(() => {
+		if (isSuccess) {
+			triggerConfetti();
+		}
+	}, [isSuccess]);
 
 	return (
 		<div className="min-h-screen bg-background px-4 py-6">
 			<div className="container mx-auto max-w-sm">
 				<Title className="text-center">Nueva RM</Title>
-				<AddMarkForm onSubmit={onSubmit} isPending={false} />
-				{/* <Toast type="error" title="Error añadiendo movimiento" description={error?.message} open={!!error} /> */}
+				<AddMarkForm onSubmit={onSubmit} isPending={isPending} />
+				<Toast type="error" title="Error añadiendo movimiento" description={error?.message} open={!!error} />
 			</div>
 		</div>
 	);
